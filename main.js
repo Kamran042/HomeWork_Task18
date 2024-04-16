f521c9a7d9cb6eef03692da5fc3d6a3379e66576
Kamran042 <tu6gsu4nn@code.edu.az>
2024-04-17 03:04:11 +0400

    finsih commit

--- main.js
+++ main.js
@@ -1,54 +1,31 @@
-const inpValue = document.getElementById("inpValue");
-const addBtn = document.getElementById("addBtn");
-const cbxInp = document.querySelector("#cbxInp");
-const list = document.getElementById("list");
+const buttonAdd = document.getElementById('addBtn');
+const inpValue = document.getElementById('inpValue');
+const listJS = document.getElementById('list');
 
-addBtn.addEventListener("click", () => {
-  if (inpValue.value == "") {
-    alert("Inpute deyer verin");
-  } else {
-    list.innerHTML += `
-    <div id="list__card">
-            <div class="list__card__left">
-                <div class="checkbox-wrapper-45">
-                    <input id="cbxInp" type="checkbox" data-action="check"/>
-                    <label class="cbx" for="cbxInp">
-                      <div class="flip">
-                        <div class="front"></div>
-                        <div class="back">
-                          <svg width="16" height="14" viewBox="0 0 16 14">
-                            <path d="M2 8.5L6 12.5L14 1.5"></path>
-                          </svg>
-                        </div>
-                      </div>
-                    </label>
-                  </div>                   
-                <p>${inpValue.value}</p>
-            </div>
-            <div class="list__card__right">
-                <button id="deleteCard" data-action="delete">
-                    <i class="fa-regular fa-trash-can"></i>
-                </button>
-            </div>
+buttonAdd.addEventListener('click',(e)=>{
+    if (inpValue.value == "") {
+        alert('Inputa deyer verin ')
+    }else{
+        listJS.innerHTML+=`
+        <div class="list__card">
+            <input type="checkbox"data-action="check" name="" id="checkboxINp" />
+            <p>${inpValue.value}</p>
+            <button data-action="delete" class="btn_card"><i class="fa-regular fa-trash-can"></i></button>
         </div>
-    `;
-    inpValue.value = "";   
-  }
-});
-
-list.addEventListener("click", (e) => {
-    if (e.target.dataset.action === "delete") {
-        e.target.parentElement.parentElement.style.display = "none";
+        `
+        inpValue.value = ''
     }
-});
+})
 
-list.addEventListener("click", (e) => {
-    if (e.target.dataset.action === "check") {
-        const paragraph = e.target.parentElement.nextElementSibling;
-        console.log(paragraph); 
-        paragraph.classList.toggle("inpCheckP"); 
+
+listJS.addEventListener('click',(e) =>{
+    if(e.target.dataset.action === "delete"){
+        e.target.parentElement.style.display = "none"
     }
-});
+})
 
-
-
+listJS.addEventListener('click', (e) =>{
+    if(e.target.dataset.action === "check"){
+        e.target.nextElementSibling.classList.toggle('inpCheckP')
+    }
+})
